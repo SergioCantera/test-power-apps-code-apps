8b30c96fc163a8cdb7155c496acc64c1cc0b1b2b
SergioCantera <sgfcantera@hotmail.com>
2026-03-01 21:20:05 +0100

    feat: agregar esquemas de datos para la gestión de tareas y actualizar .gitignore

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "siero_todos": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "siero_todoid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
